3de12e41bd83469ca1d435c87e0d73ba15beb842
Jose Belmin <113086888+JoseBelmin@users.noreply.github.com>
2024-01-12 20:22:30 +0400

    Day1

--- movie_app.py
+++ movie_app.py
@@ -1,25 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
+from tkinter import font
 
 # API Key
 ## f38ca6a78610b61ecefac1679d06c740
 
-# Global variables
-font = ("Inter", 16)
-
 # Create the main window
-window = tk.Tk()
-window.title("Mango Movies")
+window = ctk.CTk()
 window.geometry("1133x744")
-window.option_add("*font", font)
+window.title("Mango Movies")
+
+# Font Families
+Lexend = font.Font(family="Lexend", size=12)
+Inter = font.Font(family="Inter", size=12)
+
 
 # Logo image
-logoImg = Image.open("Images\logo.png")
+logoImg = Image.open("Icons\logo.png")
 logo_tk = ImageTk.PhotoImage(logoImg)
 
-logoLabel = ttk.Label(window, image=logo_tk)
+logoLabel = ttk.Label(window, 
+                      image=logo_tk)
 logoLabel.pack()
 
 # Search bar
@@ -28,14 +31,19 @@ def search():
     # Search logic here
     print(f"{query}")
 # Entry field
-searchBar = ctk.CTkEntry(window, width=50)
+searchBar = ttk.Entry(window, 
+                      width=50)
 searchBar.pack()
 # Search Button
-searchButton = ctk.CTkButton(window, text="Search", command=search)
-searchButton.pack()
+searchImg = Image.open("Icons\Search.png").resize((45, 45))
+search_tk = ImageTk.PhotoImage(searchImg)
 
-label = ctk.CTkLabel(window, text="Hello, World!")
-label.pack()
+searchButton = ttk.Button(window, 
+                          text="Search", 
+                          command=search,
+                          takefocus=False,
+                          image=search_tk)
+searchButton.pack()
 
 
 
