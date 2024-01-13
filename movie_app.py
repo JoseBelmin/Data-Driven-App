49c00e7e08fbdd42fa0e74fdb816f5c90b76f3b1
Jose Belmin <113086888+JoseBelmin@users.noreply.github.com>
2024-01-13 06:44:28 +0400

    Simplify

--- movie_app.py
+++ movie_app.py
@@ -9,43 +9,38 @@ from tkinter.font import Font
 
 # Create the main window
 window = tk.Tk()
-window.minsize(1133, 744)
-window.title("Mango Movies")
-
-# Background image
-bgImg = Image.open("Assets\Background.png")
-bg_tk = ImageTk.PhotoImage(bgImg)
-
-bgLabel = ttk.Label(window,
-                    image=bg_tk)
-bgLabel.place(x=0, y=0, relwidth=1, relheight=1)
+window.geometry("1133x744")
+window.resizable(False, False)
+window["background"] = "#CBF3F0"
+window.title("logo Movies")
 
 # Font Families
 Lexend = Font(family="Lexend", size=12)
 Inter = Font(family="Inter", size=12)
 
-# Logo image
-logoImg = Image.open("Assets\logo.png").resize((150, 100))
+# Mango logo
+logoImg = Image.open("Assets\logo.png").resize((216, 144))
 logo_tk = ImageTk.PhotoImage(logoImg)
 
 logoLabel = ttk.Label(window, 
-                      image=logo_tk)
-logoLabel.pack(side="left", anchor="nw", fill="x", expand=True, padx=(30, 0), pady=(0, 0))
+                      image=logo_tk,
+                      background="#CBF3F0")
+logoLabel.pack()
 
 # Search bar
 def search():
     query = searchBar.get()
-    # Search logic here
+    ## Search logic here
     print(f"{query}")
 # Entry field
 searchBar = ctk.CTkEntry(window, 
                          width=150,
-                         height=35,
+                         height=30,
                          text_color="#000000",
                          fg_color="#FFFFFF",
                          border_color="#FFFFFF",
                          corner_radius=6)
-searchBar.pack(side="left", anchor="ne", expand=False, pady=(35, 0))
+searchBar.pack()
 # Search Button
 searchButton = ctk.CTkButton(window, 
                              text="Search",
@@ -53,18 +48,11 @@ searchButton = ctk.CTkButton(window,
                              font=(Lexend, 12, "bold"),
                              command=search,
                              width=70,
-                             height=35,
+                             height=30,
                              fg_color="#FF9F1C",
                              hover_color="#FFBF69",
                              corner_radius=6)
-searchButton.pack(side="right", anchor="ne", expand=False, padx=(0, 30), pady=(35, 0))
-
-# parent
-parent = ttk.Frame(window)
-parent.rowconfigure(10, weight=1)
-parent.columnconfigure(10, weight=1)
-parent.pack(fill="both", expand=True)
-
+searchButton.pack()
 
 
 
